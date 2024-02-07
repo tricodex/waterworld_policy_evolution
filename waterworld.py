c202b463f763c8862ff97b2e02ec5c45041a252d
pattckc <71180317+pattckc@users.noreply.github.com>
2024-02-07 01:41:57 +0100

    Update waterworld.py

--- waterworld.py
+++ waterworld.py
@@ -1,4 +1,121 @@
 # waterworld.py
+
+
+from gymnasium.utils import EzPickle
+
+from pettingzoo import AECEnv
+from waterworld_base import FPS
+from waterworld_base import WaterworldBase as _env
+from pettingzoo.utils import agent_selector, wrappers
+from pettingzoo.utils.conversions import parallel_wrapper_fn
+
+
+def env(**kwargs):
+    env = raw_env(**kwargs)
+    env = wrappers.ClipOutOfBoundsWrapper(env)
+    env = wrappers.OrderEnforcingWrapper(env)
+    return env
+
+
+parallel_env = parallel_wrapper_fn(env)
+
+
+class raw_env(AECEnv, EzPickle):
+    metadata = {
+        "render_modes": ["human", "rgb_array"],
+        "name": "waterworld_v4",
+        "is_parallelizable": True,
+        "render_fps": FPS,
+    }
+
+    def __init__(self, *args, **kwargs):
+        EzPickle.__init__(self, *args, **kwargs)
+        AECEnv.__init__(self)
+        self.env = _env(*args, **kwargs)
+
+        self.agents = ["pursuer_" + str(r) for r in range(self.env.num_agents)]
+        self.possible_agents = self.agents[:]
+        self.agent_name_mapping = dict(zip(self.agents, list(range(self.num_agents))))
+        self._agent_selector = agent_selector(self.agents)
+
+        # spaces
+        self.action_spaces = dict(zip(self.agents, self.env.action_space))
+        self.observation_spaces = dict(zip(self.agents, self.env.observation_space))
+        self.has_reset = False
+
+        self.render_mode = self.env.render_mode
+
+    def get_individual_rewards(self, agent):
+        if self.has_reset:
+            agent_id = self.agent_name_mapping[agent]
+            return self.env.get_individual_rewards(agent_id)
+        else:
+            return None
+
+    def observation_space(self, agent):
+        return self.observation_spaces[agent]
+
+    def action_space(self, agent):
+        return self.action_spaces[agent]
+
+    def convert_to_dict(self, list_of_list):
+        return dict(zip(self.agents, list_of_list))
+
+    def reset(self, seed=None, options=None):
+        if seed is not None:
+            self.env._seed(seed=seed)
+        self.has_reset = True
+        self.env.reset()
+        self.agents = self.possible_agents[:]
+        self._agent_selector.reinit(self.agents)
+        self.agent_selection = self._agent_selector.next()
+        self.rewards = dict(zip(self.agents, [(0) for _ in self.agents]))
+        self._cumulative_rewards = dict(zip(self.agents, [(0) for _ in self.agents]))
+        self.terminations = dict(zip(self.agents, [False for _ in self.agents]))
+        self.truncations = dict(zip(self.agents, [False for _ in self.agents]))
+        self.infos = dict(zip(self.agents, [{} for _ in self.agents]))
+
+    def close(self):
+        if self.has_reset:
+            self.env.close()
+
+    def render(self):
+        return self.env.render()
+
+    def step(self, action):
+        if (
+            self.terminations[self.agent_selection]
+            or self.truncations[self.agent_selection]
+        ):
+            self._was_dead_step(action)
+            return
+
+        agent = self.agent_selection
+        is_last = self._agent_selector.is_last()
+        self.env.step(action, self.agent_name_mapping[agent], is_last)
+
+        for r in self.rewards:
+            self.rewards[r] = self.env.control_rewards[self.agent_name_mapping[r]]
+        if is_last:
+            for r in self.rewards:
+                self.rewards[r] += self.env.last_rewards[self.agent_name_mapping[r]]
+
+        if self.env.frames >= self.env.max_cycles:
+            self.truncations = dict(zip(self.agents, [True for _ in self.agents]))
+        else:
+            self.terminations = dict(zip(self.agents, self.env.last_dones))
+
+        self._cumulative_rewards[self.agent_selection] = 0
+        self.agent_selection = self._agent_selector.next()
+        self._accumulate_rewards()
+
+        if self.render_mode == "human":
+            self.render()
+
+    def observe(self, agent):
+        return self.env.observe(self.agent_name_mapping[agent])
+    
+    
 """
 # Waterworld
 
@@ -135,119 +252,3 @@ thrust_penalty=-0.5, local_ratio=1.0, speed_features=True, max_cycles=500)
 * v0: Initial versions release (1.0.0)
 
 """
-
-from gymnasium.utils import EzPickle
-
-from pettingzoo import AECEnv
-from waterworld_base import FPS
-from waterworld_base import WaterworldBase as _env
-from pettingzoo.utils import agent_selector, wrappers
-from pettingzoo.utils.conversions import parallel_wrapper_fn
-
-
-def env(**kwargs):
-    env = raw_env(**kwargs)
-    env = wrappers.ClipOutOfBoundsWrapper(env)
-    env = wrappers.OrderEnforcingWrapper(env)
-    return env
-
-
-parallel_env = parallel_wrapper_fn(env)
-
-
-class raw_env(AECEnv, EzPickle):
-    metadata = {
-        "render_modes": ["human", "rgb_array"],
-        "name": "waterworld_v4",
-        "is_parallelizable": True,
-        "render_fps": FPS,
-    }
-
-    def __init__(self, *args, **kwargs):
-        EzPickle.__init__(self, *args, **kwargs)
-        AECEnv.__init__(self)
-        self.env = _env(*args, **kwargs)
-
-        self.agents = ["pursuer_" + str(r) for r in range(self.env.num_agents)]
-        self.possible_agents = self.agents[:]
-        self.agent_name_mapping = dict(zip(self.agents, list(range(self.num_agents))))
-        self._agent_selector = agent_selector(self.agents)
-
-        # spaces
-        self.action_spaces = dict(zip(self.agents, self.env.action_space))
-        self.observation_spaces = dict(zip(self.agents, self.env.observation_space))
-        self.has_reset = False
-
-        self.render_mode = self.env.render_mode
-
-    def get_individual_rewards(self, agent):
-        if self.has_reset:
-            agent_id = self.agent_name_mapping[agent]
-            return self.env.get_individual_rewards(agent_id)
-        else:
-            return None
-
-    def observation_space(self, agent):
-        return self.observation_spaces[agent]
-
-    def action_space(self, agent):
-        return self.action_spaces[agent]
-
-    def convert_to_dict(self, list_of_list):
-        return dict(zip(self.agents, list_of_list))
-
-    def reset(self, seed=None, options=None):
-        if seed is not None:
-            self.env._seed(seed=seed)
-        self.has_reset = True
-        self.env.reset()
-        self.agents = self.possible_agents[:]
-        self._agent_selector.reinit(self.agents)
-        self.agent_selection = self._agent_selector.next()
-        self.rewards = dict(zip(self.agents, [(0) for _ in self.agents]))
-        self._cumulative_rewards = dict(zip(self.agents, [(0) for _ in self.agents]))
-        self.terminations = dict(zip(self.agents, [False for _ in self.agents]))
-        self.truncations = dict(zip(self.agents, [False for _ in self.agents]))
-        self.infos = dict(zip(self.agents, [{} for _ in self.agents]))
-
-    def close(self):
-        if self.has_reset:
-            self.env.close()
-
-    def render(self):
-        return self.env.render()
-
-    def step(self, action):
-        if (
-            self.terminations[self.agent_selection]
-            or self.truncations[self.agent_selection]
-        ):
-            self._was_dead_step(action)
-            return
-
-        agent = self.agent_selection
-        is_last = self._agent_selector.is_last()
-        self.env.step(action, self.agent_name_mapping[agent], is_last)
-
-        for r in self.rewards:
-            self.rewards[r] = self.env.control_rewards[self.agent_name_mapping[r]]
-        if is_last:
-            for r in self.rewards:
-                self.rewards[r] += self.env.last_rewards[self.agent_name_mapping[r]]
-
-        if self.env.frames >= self.env.max_cycles:
-            self.truncations = dict(zip(self.agents, [True for _ in self.agents]))
-        else:
-            self.terminations = dict(zip(self.agents, self.env.last_dones))
-
-        self._cumulative_rewards[self.agent_selection] = 0
-        self.agent_selection = self._agent_selector.next()
-        self._accumulate_rewards()
-
-        if self.render_mode == "human":
-            self.render()
-
-    def observe(self, agent):
-        return self.env.observe(self.agent_name_mapping[agent])
-    
-    
